897f1b060b15e74df66df0fc8395775e02bfb6c8
DorivalMega <dorival@megamodel.com.br>
2024-04-05 17:13:36 -0300

    Completado a aula 17 - modulo 2

--- modulo2/assets/js/script.js
+++ modulo2/assets/js/script.js
@@ -242,3 +242,39 @@ console.log(carros);
 
 //4. Exiba quantos itens tem no array
 console.log(`4. Itens no array: ${carros.length}`);
+
+// Aula 16 - O que são objetos?
+
+let personagem = {
+    nome: 'Dorival',
+    idade: 90,
+    pais: 'Brasil',
+    olhos: ['preto', 'azul'],
+    caracteristicas: {
+        forca: 20,
+        magia: 5,
+        stamina: 15
+    }
+}
+
+console.log(`O ${personagem.nome} tem ${personagem.idade} anos.`);
+
+console.log(`Ele tem ${personagem.caracteristicas.forca} de força, ${personagem.caracteristicas.magia} de magia e ${personagem.caracteristicas.stamina} de stamina.`);
+
+// Aula 17 - Acessando e alterando objetos
+
+personagem.nome = 'Pedro';
+personagem.caracteristicas.forca += 5;
+personagem.olhos.push('verde');
+console.log(personagem);
+
+let personagem2 = {
+    nome: 'Bonieky',
+    idade: 90,
+    carros: [
+        {modelo: 'Fiat', cor: 'preto'},
+        {modelo: 'Ferrari', cor: 'vermelho'}
+    ]
+}
+
+console.log(personagem2.carros[0].cor);
